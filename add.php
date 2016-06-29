c2d72244d66aa99d56526be10a91f222996cab5b
maggielai <frankyinglai@163.com>
2016-06-29 12:08:00 +0800

    xiugai

--- add.php
+++ add.php
@@ -5,13 +5,7 @@ public function postone(){
 }
 
 public function posttwo(){
-   echo '1111'
 return 111;
-}
-
-public function postthree(){
-  echo 222;
-return 2222;
 }
 
 public function postfour(){
