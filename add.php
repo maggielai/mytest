64baa59a497d2bcdbd3b7edf2a1046f4185a5798
maggielai <frankyinglai@163.com>
2016-06-29 10:53:47 +0800

    增加文件ｔｗｏ

--- add.php
+++ add.php
@@ -5,9 +5,13 @@ public function postone(){
 }
 
 public function posttwo(){
+   echo '1111'
+return 111;
 }
 
 public function postthree(){
+  echo 222;
+return 2222;
 }
 
 public function postfour(){
